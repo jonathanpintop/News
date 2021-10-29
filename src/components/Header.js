2b9916d5600c990ce944c5530c05f6cae2a60375
Jonathan Pinto <jonathanpintopp@gmail.com>
2021-10-29 09:31:23 -0500

    feat: Header component added

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = ({titulo}) => {
+    return (
+      <nav className="nav-wrapper light-blue darken-3">
+          <a href="#!" className="brand-logo center">{titulo}</a>
+
+
+<div className="container white">
+    <h1>Formulario aqui</h1>
+</div>
+
+      </nav>
+    )
+}
+
+export default Header
